be6ac25b92b25422eb109aa6b270e02954b3214a
Yuval <nisim.yuval41@gmail.com>
2026-01-15 10:33:08 +0000

    connectionImpl class creation

--- server/src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
+++ server/src/main/java/bgu/spl/net/srv/ConnectionsImpl.java
@@ -1,5 +1,7 @@
 package bgu.spl.net.srv;
-
-public class ConnectionImpl {
-
+//new class implemented according to page 9 of the assignemnt
+public class ConnectionsImpl<T> implements Connections<T> {
+    public boolean send(int connectionId, T msg){ return false; }
+    public void send(String channel, T msg){}
+    public void disconnect(int connectionId){}
 }
