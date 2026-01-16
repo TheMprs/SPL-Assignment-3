dd59d1391f84a0cfc6b554c2c3c2c71daae1285e
Yuval <nisim.yuval41@gmail.com>
2026-01-16 18:07:13 +0200

    server beggining

--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -1,8 +1,24 @@
 package bgu.spl.net.impl.stomp;
 
+import bgu.spl.net.srv.Server;
+import bgu.spl.net.srv.Reactor;
+
 public class StompServer {
 
     public static void main(String[] args) {
         // TODO: implement this
+        int port = Integer.parseInt(args[0]);
+        String serverType = args[1];
+
+        if(serverType.equals("tpc")){
+            Server.threadPerClient(
+                port, 
+                StompMessagingProtocolImpl::new, 
+                StompEncoderDecoder::new
+            ).serve();
+        }
+        
+        else if(serverType.equals("reactor")){
+        }
     }
 }
